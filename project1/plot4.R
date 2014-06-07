14bde1d913e6d1ecca878869be8b0970dce4ce92
Limbu Malongo Limbu <limbu.malongo@gmail.com>
2014-06-07 04:17:30 +0300

    project1 final commit

---
+++ project1/plot4.R
@@ -0,0 +1,120 @@
+#This assignment uses data from the UC Irvine Machine Learning Repository, a popular repository for 
+#machine learning datasets. In particular, we will be using the “Individual household electric power 
+#consumption Data Set” which I have made available on the course web site:
+#Dataset: Electric power consumption [20Mb]
+#https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+#
+#Unzip the downloaded data to the working folder and make sure the file name is
+#household_power_consumption.txt
+
+
+#Loading the data
+#Calculating Memory Requirements
+#https://github.com/jtleek/modules/blob/master/02_RProgramming/reading_data_I/index.md
+
+#Data frame with 2,075,259 rows and 9 columns, 
+#assumption ==> all of which are numeric and as.Date (each numeric data and as.Date occupy 8 bytes of memory). Roughly, 
+#how much memory is required to store this data frame?
+
+
+#2,075,259 × 9 × 8 bytes/numeric
+
+#Uncomment the following if you want to prove the value above
+#2075259*9*8 #bytes
+
+# = 149,418,648 bytes
+
+#2075259*9*8/2^{20} ## Megabytes
+
+#Hence the space in memory required to handle the data frame is 142.4967 Megabytes
+
+#Reading large tables into R
+#If you are interested to know how this trick works follow the link below
+#http://www.biostat.jhsph.edu/~rpeng/docs/R-large-tables.html
+#
+#Reading the first 5 rows so that to determine the classes of the columns
+#Missing values are coded as ? hence the option ==> na.strings = '?'
+
+housepowercons5rows <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = '?', nrows = 5)
+classes <- sapply(housepowercons5rows, class)
+
+#Reading all the rows with colClasses option to improve perfomance of reading
+
+housepowercons <- read.table("household_power_consumption.txt", header = TRUE, colClasses = classes, sep = ";", na.strings = '?')
+
+#Remove housepowercons5rows, its job is over
+rm("housepowercons5rows")
+
+#Examining the data frame housepowercons
+
+#The dataset has 2,075,259 rows and 9 columns.
+dim(housepowercons)
+
+names(housepowercons)
+str(housepowercons)
+head(housepowercons)
+summary(housepowercons)
+head(housepowercons)
+tail(housepowercons)
+
+#We will only be using data from the dates 2007-02-01 and 2007-02-02
+#Coverting the Date variable to Date class
+housepowercons$Date <- as.Date(housepowercons$Date, format='%d/%m/%Y')
+class(housepowercons$Date)
+
+#Subsetting data for dates 2007-02-01 and 2007-02-02
+sub_housepowercons = housepowercons[(housepowercons$Date==as.Date('2007-02-01')|housepowercons$Date==as.Date('2007-02-02')),]
+head(sub_housepowercons)
+tail(sub_housepowercons)
+
+#Creating the variable for x-axis
+#Create another variable datetime which combines Date and Time
+sub_housepowercons$datetime = paste(sub_housepowercons$Date, sub_housepowercons$Time)
+class(sub_housepowercons$datetime)
+head(sub_housepowercons$datetime)
+
+#Setting the class of datetime variable to POSIX date/time
+sub_housepowercons$datetime = strptime(sub_housepowercons$datetime, format = '%Y-%m-%d %H:%M:%S')
+class(sub_housepowercons$datetime)
+head(sub_housepowercons$datetime)
+
+timeintervals=as.POSIXct(sub_housepowercons$datetime)
+timeintervals=seq(as.POSIXct("2007-02-01 00:00:00"),by="min",length.out=length(sub_housepowercons$datetime))
+tail(timeintervals)
+
+
+#Setting the drawing region 2 rows by 2 columns row wise so as accomodate 4 plots
+
+##Set the new parameters
+par(mfrow = c(2,2))
+par(mar = c(4, 4, 2, 2))
+
+
+
+# make the top left plot
+with(sub_housepowercons, plot(timeintervals, Global_active_power, type='l', main='', xlab='', ylab='Global Active Power (kilowatts)'))
+
+
+# make the top right plot
+with(sub_housepowercons, plot(timeintervals, Voltage, type='l', main='', xlab='datetime', ylab='Voltage'))
+
+
+
+#make the down left plot
+with(sub_housepowercons, plot(timeintervals, Sub_metering_1, type='l', xlab='', ylab='Energy sub metering'))
+with(sub_housepowercons, points(timeintervals, Sub_metering_2, type='l', col = "red"))
+with(sub_housepowercons, points(timeintervals, Sub_metering_3, type='l', col = "blue"))
+legend("topright",lty="solid", bty = "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+#make the down right plot
+with(sub_housepowercons, plot(timeintervals, Global_reactive_power, type='l', main='', xlab='datetime' ))
+
+
+#Copy the plot4 from screen device to file plot4.png in working directory
+dev.copy(png, file = "plot4.png") ## Copy my plot to a PNG file
+dev.off()
+
+#Restore the default drawing region parameters
+par(mfcol = c(1,1))
+par(mar = c(5.1, 4.1, 4.1, 2.1))
